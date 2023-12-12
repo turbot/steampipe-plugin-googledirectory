65f4af16d95b498568205f41adc4a8e0cce995c4
Madhushree Ray <madhushree@turbot.com>
2023-12-12 23:08:16 +0530

    Removed config schema

--- googledirectory/plugin.go
+++ googledirectory/plugin.go
@@ -25,7 +25,6 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 		},
 		ConnectionConfigSchema: &plugin.ConnectionConfigSchema{
 			NewInstance: ConfigInstance,
-			Schema:      ConfigSchema,
 		},
 		TableMap: map[string]*plugin.Table{
 			"googledirectory_domain":          tableGoogleDirectoryDomain(ctx),
